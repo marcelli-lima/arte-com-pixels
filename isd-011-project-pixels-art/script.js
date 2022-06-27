3ca0d7e3d517ff420613896118734253393c7eae
marcelli-lima <marcelli_lima@hotmail.com>
2022-06-27 19:55:28 -0300

    requisitos obrigatorios finalizados

--- isd-011-project-pixels-art/script.js
+++ isd-011-project-pixels-art/script.js
@@ -4,4 +4,31 @@ window.onload = function() {
   color[1].style.background = "red"
   color[2].style.background = "blue"
   color[3].style.background = "purple"
-} 
+
+  function handleChangeSelected(event) {
+    const select = document.getElementsByClassName("selected")[0]
+    select.classList.remove("selected");
+    event.target.classList.add('selected');
+  }
+  const colorPalette = document.getElementById("color-palette")
+  colorPalette.addEventListener('click', handleChangeSelected);
+}
+
+const pixel = document.querySelector("#pixel-board");
+pixel.addEventListener('click', paintPixel);
+
+function paintPixel (event) {
+const selected = document.querySelector(".selected");
+event.target.style.backgroundColor = selected.style.backgroundColor
+}
+
+const clear = document.querySelector("#clear-board")
+clear.addEventListener("click", clearTable)
+
+function clearTable() {
+  const getPixels = document.querySelectorAll('.pixel');
+  for (let pixel of getPixels) {
+    pixel.style.backgroundColor = 'white';
+  }
+}
+
