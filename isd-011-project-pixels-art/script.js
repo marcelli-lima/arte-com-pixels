d5ad1d1ac76511e14914346cf1db9a7f98f66b08
marcelli-lima <marcelli_lima@hotmail.com>
2022-06-27 19:49:12 -0300

    requisito 4 finalizado

---
+++ isd-011-project-pixels-art/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  const color = document.getElementsByClassName("color")
+  color[0].style.background = "black"
+  color[1].style.background = "red"
+  color[2].style.background = "blue"
+  color[3].style.background = "purple"
+} 
