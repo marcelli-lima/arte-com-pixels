81078eafdaaf2222269d26d55253370c5f3738d8
marcelli-lima <marcelli_lima@hotmail.com>
2022-06-28 13:53:16 -0300

    correçoes do lint finalizadas

--- isd-011-project-pixels-art/script.js
+++ isd-011-project-pixels-art/script.js
@@ -1,38 +1,36 @@
-window.onload = function() {
-  const color = document.getElementsByClassName("color")
-  color[0].style.background = "black"
-  color[1].style.background = '#' + Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0');
-  color[2].style.background = '#' + Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0');
-  color[3].style.background = '#' + Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0');
-
-  function handleChangeSelected(event) {
-    const select = document.getElementsByClassName("selected")[0]
-    select.classList.remove("selected");
-    event.target.classList.add('selected');
-  }
-  const colorPalette = document.getElementById("color-palette")
-  colorPalette.addEventListener('click', handleChangeSelected);
-
-
-const pixel = document.querySelector("#pixel-board");
-pixel.addEventListener('click', paintPixel);
-
-function paintPixel (event) {
-const selected = document.querySelector(".selected");
-event.target.style.backgroundColor = selected.style.backgroundColor
+window.onload = function () {
+  const color = document.getElementsByClassName('color');
+  color[0].style.background = 'black';
+  color[1].style.background = `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0')}`;
+  color[2].style.background = `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0')}`;
+  color[3].style.background = `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0')}`;
+};
+function handleChangeSelected(event) {
+  const select = document.getElementsByClassName('selected')[0];
+  select.classList.remove('selected');
+  event.target.classList.add('selected');
 }
+const colorPalette = document.getElementById('color-palette');
+colorPalette.addEventListener('click', handleChangeSelected);
 
-const clear = document.querySelector("#clear-board")
-clear.addEventListener("click", clearTable)
+function paintPixel(event) {
+  const selected = document.querySelector('.selected');
+  const evenT = event.target;
+  evenT.style.backgroundColor = selected.style.backgroundColor
+}
+const pixel = document.querySelector('#pixel-board');
+pixel.addEventListener('click', paintPixel);
 
 function clearTable() {
   const getPixels = document.querySelectorAll('.pixel');
-  for (let pixel of getPixels) {
-    pixel.style.backgroundColor = 'white';
+  for (const pixe of getPixels) {
+    pixe.style.backgroundColor = 'white';
   }
 }
+const clear = document.querySelector('#clear-board');
+clear.addEventListener('click', clearTable);
 
-const board = document.getElementById("generate-board")
+const board = document.getElementById('generate-board');
 
 function removeTable() {
   const lines = document.querySelectorAll('tr');
@@ -45,30 +43,33 @@ function removeTable() {
   }
 }
 
-board.addEventListener("click", function() {
-let input = document.getElementById("board-size")
-if (input.value === "") {
-  alert("Board inválido!")
-}
-if (input.value !== '') {
-  removeTable();
-let boardSize;
-if (input.value < 5) {
-  boardSize = 5;
-} else if (input.value > 50) {
-  boardSize = 50;
-} else {
-  boardSize = input.value;
-}
-for (let linIndex = 0; linIndex < boardSize; linIndex += 1) {
-  let line = document.createElement('tr');
-  pixel.appendChild(line);
-  for (let colIndex = 0; colIndex < boardSize; colIndex += 1) {
-    let column = document.createElement('td');
-    column.className = 'pixel';
-    line.appendChild(column);
+function createTable(size) {
+  for (let linIndex = 0; linIndex < size; linIndex += 1) {
+    const line = document.createElement('tr');
+    pixel.appendChild(line);
+    for (let colIndex = 0; colIndex < size; colIndex += 1) {
+      const column = document.createElement('td');
+      column.className = 'pixel';
+      line.appendChild(column);
+    }
   }
 }
-}
-})
-} 
+
+board.addEventListener('click', () => {
+  const input = document.getElementById('board-size');
+  if (input.value === '') {
+    alert('Board inválido!');
+  }
+  removeTable();
+  let boardSize;
+  if (input.value < 5) {
+    boardSize = 5;
+    createTable(boardSize);
+  } else if (input.value > 50) {
+    boardSize = 50;
+    createTable(boardSize);
+  } else {
+    boardSize = input.value;
+    createTable(boardSize);
+  }
+});
